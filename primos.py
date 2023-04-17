f85ed492ec9e0e25b58004ff83f87e79ead13429
deywsjhow <jonathan.deyws@aluno.faculdadeimpacta.com.br>
2023-04-17 19:54:56 -0300

    primos

---
+++ primos.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def numeros_primos():
+    qtdTotal = 100
+    primos = "1,2,"
+    candPrimo = 3
+    qtdEncontrados = 2
+    ehPrimo = 1
+
+    while qtdEncontrados < qtdTotal:
+        for i in range(2, candPrimo):
+            if candPrimo % i == 0:
+                ehPrimo = 0
+                break
+        if ehPrimo == 1:
+            primos = primos + str(candPrimo) + ','
+            qtdEncontrados +=1
+        ehPrimo = 1    
+        candPrimo += 1
+    return primos
+            
+
+    
+
+if __name__ == "__main__": 
+    app.run(debug=True)
